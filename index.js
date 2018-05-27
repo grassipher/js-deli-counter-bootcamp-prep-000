306ef088906e19a922a0af27507659a36f5fd3e5
Gabriel Valle <gvalle91@gmail.com>
2018-05-27 16:19:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeli, newPerson){
   var position = katzDeli.indexOf(newPerson)+1;
   return `Welcome, ${newPerson}. You are number ${position} in line.`;
 }
+function nowServing(){
+  if (katzDeli.length>0){
+    var firstPerson = katzDeli[1];
+    delete katzDeli[1];
+    return `Currently serving ${firstPerson}`;
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
