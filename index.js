04213713f0320ee935967454e007a10be09d4ba8
Gabriel Valle <gvalle91@gmail.com>
2018-05-27 16:20:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var katzDeli = [];
-function takeANumber(katzDeli, newPerson){
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, newPerson){
   katzDeli.push(newPerson);
   var position = katzDeli.indexOf(newPerson)+1;
   return `Welcome, ${newPerson}. You are number ${position} in line.`;
 }
-function nowServing(){
+function nowServing(katzDeliLine){
   if (katzDeli.length>0){
-    var firstPerson = katzDeli[1];
-    delete katzDeli[1];
+    var firstPerson = katzDeliLine[1];
+    delete katzDeliLine[1];
     return `Currently serving ${firstPerson}`;
   } else {
     return "There is nobody waiting to be served!";
