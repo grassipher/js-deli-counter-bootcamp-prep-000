ca3c54afed0724362623c801193417fe914c8e9c
Gabriel Valle <gvalle91@gmail.com>
2018-05-27 16:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ function nowServing(katzDeli){
   if (katzDeli.length>0){
     var firstPerson = katzDeli[0];
     delete katzDeli[0];
-    return `Currently serving ${firstPerson}`;
+    return `Currently serving ${firstPerson}.`;
   } else {
     return "There is nobody waiting to be served!";
+  }
+}
+function currentLine(katzDeli){
+  if (katzDeli.length>0){
+    var line;
+    for (var i = 0; i < katzDeli.length; i++){
+      if (i === katzDeli.length){
+        line = `${i}. ${katzDeli[i]}`;
+      } else {
+        line = `${i}. ${katzDeli[i]}, `;
+      }
+    }
+    return `The line is currently: ${line}`;
+  } else {
+    return "The line is currently empty.";
   }
 }
