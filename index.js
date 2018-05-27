ed8fd78789e7d5d5adc9f0219153093bcfedda74
Gabriel Valle <gvalle91@gmail.com>
2018-05-27 16:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeli = [];
 function takeANumber(katzDeli, newPerson){
-  
-  return `Welcome, ${newPerson}. You are number in line.`;
+  katzDeli.push(newPerson);
+  var position = katzDeli.indexOf(newPerson)+1;
+  return `Welcome, ${newPerson}. You are number ${position} in line.`;
 }
